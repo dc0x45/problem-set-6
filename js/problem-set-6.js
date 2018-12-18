8f2493ab348d9d8a75af366b794008c511042b3f
dcote007 <32779137+dcote007@users.noreply.github.com>
2018-12-17 22:38:16 -0500

    final (I think!)

--- js/problem-set-6.js
+++ js/problem-set-6.js
@@ -12,6 +12,11 @@
 
 function sayHello() {
 
+  let hello = document.getElementById('canvas1').getContext('2d');
+  hello.clearRect(0, 0, canvas1.width, canvas1.height);
+  hello.font = "48px sans-serif";
+  hello.strokeText('Hello, World!', 10, 50);
+  
 }
 
 /*
@@ -38,7 +43,45 @@ function sayHello() {
  */
 
 function drawRectangle() {
+  
+  let width = prompt("Width:");
+  let height = prompt("Height:");
+  let x = prompt("X:");
+  let y = prompt("Y:");
 
+  if (Number.isInteger(Number(width)) == false || Number.isInteger(Number(height)) == false || Number.isInteger(Number(x)) == false || Number.isInteger(Number(y)) == false) {
+    alert("Invalid input. Please try again and make sure all inputs are numbers.")
+  }
+  else if (width<1) {
+    alert("Width is too small. Please try again.");
+  }
+  else if (width>1024) {
+    alert("Width is too large. Please try again.");
+  }
+  else if (height>512) {
+    alert("Height is too large. Please try again.");
+  }
+  else if (height<1) {
+    alert("Height is too small. Please try again.");
+  }
+  else if (x<5) {
+    alert("X is too small. Please try again.");
+  }
+  else if (y<5) {
+    alert("Y is too small. Please try again.");
+  }
+  else if ((Number(width) + Number(x))>1024) {
+    alert("X is too large. Please try again.");
+  }
+  else if ((Number(height) + Number(y))>512) {
+    alert("Y is too large. Please try again.");
+  } else {
+
+  let ctx = document.getElementById('canvas2').getContext('2d');
+  ctx.clearRect(0,0,1024,512);
+  ctx.strokeRect(x,y,width,height);
+}
+}
 }
 
 /*
@@ -67,7 +110,50 @@ function drawRectangle() {
  */
 
 function drawColoredRectangle() {
-
+  const colored = document.getElementById('canvas3');
+  const ctx = colored.getContext('2d');
+  
+  ctx.clearRect(0, 0, canvas3.width, canvas3.height)
+  
+  let xcor=Number(10)
+  let ycor=Number(10)
+  let h=Number(50)
+  let w=Number(100)
+  let answer = prompt('Color:')
+  let backup = ans
+  switch(ans){
+  case answer='red':
+    ctx.fillStyle = 'red';
+    ctx.fillRect(xcor, ycor, w, h);
+    break
+  case answer='blue':
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(xcor, ycor, w, h);
+    break
+  case answer='purple':
+    ctx.fillStyle = 'purple';
+    ctx.fillRect(xcor, ycor, w, h);
+    break
+  case answer='green':
+    ctx.fillStyle = 'green';
+    ctx.fillRect(xcor, ycor, w, h);
+    break
+  case answer='black':
+    ctx.fillStyle = 'black';
+    ctx.fillRect(xcor, ycor, w, h);
+    break
+  case answer='orange':
+    ctx.fillStyle = 'orange';
+    ctx.fillRect(xcor, ycor, w, h);
+    break
+  case answer='yellow':
+    ctx.fillStyle = 'yellow';
+    ctx.fillRect(xcor, ycor, w, h);
+    break
+  default:
+    alert(backup + " is not a unsupported color.")
+    break
+  }
 }
 
 /*
@@ -100,7 +186,26 @@ function drawColoredRectangle() {
  */
 
 function drawTriangle() {
+  
+  let ctx = document.getElementById('canvas4').getContext('2d');
+  let side1 = prompt("Side 1:");
+  let side2 = prompt("Side 2:");
+  let side3 = prompt("Side 3:");
+  Number(side1)
+  Number(side2)
+  Number(side3)
+  if ((side1**2) + (side2**2) != (side3**2)){
+    alert("Invalid right triangle.")
+  } 
 
+  ctx.clearRect(0, 0, canvas4.width, canvas4.height)
+  ctx.beginPath();
+  ctx.moveTo(10, 10);
+  ctx.lineTo(10, 10+side1);
+  ctx.lineTo(10+side2, 10+side1)
+  ctx.lineTo(10, 10);
+  ctx.stroke();
+}
 }
 
 /*
@@ -123,7 +228,35 @@ function drawTriangle() {
  */
 
 function drawSmileyFace() {
+  let radius = prompt("Enter radius:");
+  Number(radius)
+  let eyes = (radius * 0.1);
+  let mouth = (radius * 0.7);
+  let end = Math.PI*2
 
+  if (radius>250) {
+    alert("Radius too large!");
+  }
+  else if (radius<1) {
+    alert("Radius too small!");
+  }
+  else {
+  let ctx = document.getElementById('canvas5').getContext('2d');
+  ctx.clearRect(0,0,1024,512);
+  ctx.beginPath();
+  ctx.arc(radius, radius, radius, 0, end)
+  ctx.stroke()
+  ctx.beginPath()
+  ctx.arc(radius, radius, mouth, 0, end/2); 
+  ctx.stroke();
+  ctx.beginPath()
+  ctx.arc(radius * 1.4, radius * 0.5, eyes, 0, end);
+  ctx.stroke();
+  ctx.beginPath()
+  ctx.arc(radius * 0.6, radius * 0.5, eyes, 0, end);
+  ctx.stroke();
+ }
+}
 }
 
 /*
@@ -146,6 +279,38 @@ function drawSmileyFace() {
 
 function drawStar() {
 
+  let canvas = document.getElementById("canvas6");
+  let star = canvas.getContext("2d");
+  star.clearRect(0, 0, canvas6.width, canvas6.height);
+
+  let oRadius;
+  let iRadius;
+  let degrees = 0;
+
+  do {
+    oRadius = prompt("Please input an outer radius.");
+    Number(oRadius);
+  } while (oRadius < 0 || isNaN(oRadius) == true)
+
+  do {
+    iRadius = prompt("Enter the inner radius.");
+    Number(iRadius);
+  } while (iRadius < 0 || iRadius > oRadius || isNaN(iRadius) == true)
+
+  star.beginPath();
+  star.moveTo(125,125-oRadius);
+    for (let i=0; i<=5; i++){
+      star.lineTo(125 + Math.round((Math.cos(Math.PI * (degrees - 90) / 180) * oRadius)), 125 + Math.round((Math.sin(Math.PI * (degrees - 90) / 180) * oRadius)));
+      
+      degrees = (degrees + 36);
+      
+      star.lineTo(125 + Math.round((Math.cos(Math.PI * (degrees - 90) / 180) * iRadius)), 125 + Math.round((Math.sin(Math.PI * (degrees - 90) / 180) * iRadius)));
+      
+      degrees = (degrees + 36);
+    }
+    star.stroke();
+    star.closePath();
+
 }
 
 /*
@@ -163,8 +328,31 @@ function drawStar() {
  * The leftmost and topmost sides should have X- and Y-coordinates of 10.
  */
 
-function drawStopSign() {
-
+function drawStopSign() {    
+  
+  let ctx = document.getElementById('canvas7').getContext("2d");
+  
+  let var1 = 80;
+  let cir = Number((80/Math.sqrt(2)).toFixed(0));
+  let x = 70;
+  let y = 10;
+  
+  ctx.beginPath();
+  ctx.moveTo(x , y);
+  ctx.lineTo((x + var1) , y);
+  ctx.lineTo((x + var1 + cir) , (y + cir));
+  ctx.lineTo((x + var1 + cir) , (y + var1 + cir));
+  ctx.lineTo((x+ var1) , (y + var1 + cir + cir));
+  ctx.lineTo(x , (y + var1 + cir + cir));
+  ctx.lineTo((x - cir) , (y + var1 + cir));
+  ctx.lineTo((x - cir) , (y + cir));
+  ctx.lineTo(x , y);
+  ctx.fillStyle="#f00";
+  ctx.fill();
+  ctx.closePath();
+  ctx.fillStyle="#ffffff";
+  ctx.font="80px sans-serif";
+  ctx.fillText("STOP", 22, 133);
 }
 
 /*
@@ -186,6 +374,28 @@ function drawStopSign() {
  */
 
 function drawPyramid() {
+  let canvas = document.getElementById('canvas8');
+  let ctx = canvas.getContext('2d');
+  
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+  let sideLen = prompt("Input side length");
+  Number(sideLen)
+  let x = 10;
+  let y = (canvas.height - 10);
+  let i = 0;
+  let line = 1;
+  
+  while(i<5){
+    for(let d = 0 + line; d <= 5; d++){
+      ctx.strokeRect(x, y - sideLen, sideLen, sideLen);
+      x = (x + sideLen);
+    }
+    y -= sideLen;
+    x = (10 + (sideLen / 2) * line);
+    line++;
+    i++;
+  }
 
 }
 
@@ -219,5 +429,54 @@ function drawPyramid() {
  */
 
 function drawHouse() {
+  let canvas = document.getElementById('canvas9');
+  let ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+  while(true){
+  let doorColor=prompt("Front door color:")
+  let houseColor=prompt("House color:")
+  if((doorColor=="brown" || doorColor=="blue" || doorColor=="green" || doorColor=="orange" || doorColor=="purple" || doorColor=="red" || doorColor=="yellow")
+  && (houseColor=="brown" || houseColor=="blue" || houseColor=="green" || houseColor=="orange" || houseColor=="purple" || houseColor=="red" || houseColor=="yellow")) {
+    break;
+  }
+  else{
+      alert("Some of your colors are invalid. Please try again.")
+  }
+}
+let x=150;
+let lengthHouse=576;
+let heightHouse=400;
+let y=canvas.height-heightHouse-10;
+ctx.beginPath();
+
+ctx.fillStyle=houseColor;
+ctx.fillRect(x,y,lengthHouse,heightHouse);
+
+ctx.fillStyle=doorColor;
+ctx.fillRect(x+(lengthHouse/2)-30,y+300,60,100);
+ctx.strokeRect(x+(lengthHouse/2)-30,y+300,60,100);
+ctx.stroke();
+
+
+ctx.fillStyle="gray";
+ctx.moveTo(x,y);
+ctx.lineTo(x+286,150);
+ctx.lineTo(x+lengthHouse,y);
+ctx.lineTo(x,y);
+ctx.fill();
+
+ctx.fillStyle="#ADD8E6";
+ctx.fillRect(300,y+100, 50, 50);
+ctx.fillRect(526,y+100, 50, 50);
+ctx.fillRect(300,y+200, 50, 50);
+ctx.fillRect(526,y+200, 50, 50);
+ctx.closePath();
+
+ctx.beginPath();
+ctx.fillStyle='black';
+ctx.arc(450, 700, 6, 0, Math.PI*2);
+ctx.fill();
+ctx.closePath()
+}
 }
